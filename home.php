54703e8e7e5d633a08db6c8ac2911e6a41196733
Diego Barreiro <diego.barreiro.perez@gmail.com>
2017-07-19 00:34:18 +0200

    Added Bootstrap support
    To make it mobile friendly

--- home.php
+++ home.php
@@ -31,7 +31,7 @@
     </div> -->
 
     <!-- Loader -->
-    <div class="loader" id="CS">
+    <div class="loader" name="CS">
     	<div class="preloader-wrapper big active">
     	    <div class="spinner-layer spinner-blue-only">
     	    	<div class="circle-clipper left">
@@ -47,7 +47,7 @@
     	</div>
     </div>
     <!-- End Loader -->
-    <div class="wrapper" id="CS">
+    <div class="wrapper" name="CS">
     	<div class="main">
     		<div class="tab-content">
     			<div role="tabpanel" class="tab-pane active" id="home">
