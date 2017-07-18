b4dd7a9d23ccb045b53955114a6e58dc18f329f7
Diego Barreiro <diego.barreiro.perez@gmail.com>
2017-07-18 22:36:39 +0200

    Community will open in new tab

--- header.php
+++ header.php
@@ -1,16 +1,19 @@
+	<meta charset="utf-8" />
 	<meta name="theme-color" content="#673ab7">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-	<link rel="shortcut icon" href="/favicon.ico">	
+	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<link rel="shortcut icon" href="/favicon.ico">
 	<link href="https://fonts.googleapis.com/css?family=Amaranth" rel="stylesheet">
-	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">	
-	<link href="/css/materialize.min.css" rel="stylesheet" type="text/css">
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">	<link href="/css/materialize.min.css" rel="stylesheet" type="text/css">
 	<link href="/css/style.css" rel="stylesheet" type="text/css">
 	<script src="/js/jquery.min.js" type="text/javascript"></script>
 	<script src="/js/materialize.min.js" type="text/javascript"></script>
 	<script src="/js/fontawesome.js" type="text/javascript"></script>
 	<script src="/js/mobile-nav.js" type="text/javascript"></script>
-  </head>
-  <body>
+  </head>
+
+	<style type="text/css">body {overflow:hidden;}</style>
+	
+	<body>
 <?php include_once("config/analyticstracking.php") ?>
 	<div class="navbar-fixed">
 	<nav>
@@ -22,7 +25,7 @@
 		  <li<?php if( strpos($_SERVER["REQUEST_URI"], "/about") !== false ) { echo " class=\"active\""; }?>><a href="/about">About</a></li>
 		  <li<?php if( strpos($_SERVER["REQUEST_URI"], "/docs") !== false ) { echo " class=\"active\""; }?>><a href="/docs">Docs</a></li>
 		  <li<?php if( strpos($_SERVER["REQUEST_URI"], "/team") !== false ) { echo " class=\"active\""; }?>><a href="/team">Team</a></li>
-		  <li><a href="https://community.makeroid.tk/">Community</a></li>
+		  <li><a target="_blank" href="https://community.makeroid.tk/">Community</a></li>
 		  <li><a class="waves-effect waves-dark btn  pink accent-2" href="#">Login</a></a></li>
 		</ul>
 	  </div>
@@ -33,9 +36,8 @@
 	  <li<?php if( strpos($_SERVER["REQUEST_URI"], "/about") !== false ) { echo " class=\"active\""; }?>><a href="/about">About</a></li>
 	  <li<?php if( strpos($_SERVER["REQUEST_URI"], "/docs") !== false ) { echo " class=\"active\""; }?>><a href="/docs">Docs</a></li>
 	  <li<?php if( strpos($_SERVER["REQUEST_URI"], "/team") !== false ) { echo " class=\"active\""; }?>><a href="/team">Team</a></li>
-	  <li><a href="https://community.makeroid.tk/">Community</a></li>
+	  <li><a target="_blank" href="https://community.makeroid.tk/">Community</a></li>
 	  <li><a class="waves-effect waves-dark btn  pink accent-2" href="#">Login</a></a></li>
 	</ul>
-	<br>
-	<br>
+
 <?php include_once("config/ads.php"); ?>
