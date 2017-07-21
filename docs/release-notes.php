0dbe8393fe4950e8153329af512461409718004a
conorshipp <Conor@CSGames.co.uk>
2017-07-21 19:48:55 +0100

    Fix ul

--- docs/release-notes.php
+++ docs/release-notes.php
@@ -7,29 +7,29 @@
 <?php include "../header.php"; ?>
 	<div class="centre-arrangement">
 	  <h1>Release Notes</h1>
-	  <h4>July 2017</h4>	  <p>This is the first release of Makeroid, bugs are to be expected and features may be incomplete.</p>
+	  <h2>July 2017</h2>	  <p>This is the first release of Makeroid, bugs are to be expected and features may be incomplete.</p>
 	  <br>
 	  <p>Please report bugs, suggest features and give us feedback on our <a href="https://community.makeroid.tk/">community.</a></p>
 	  <br>
-	  <h4>Differences between MIT App Inventor and Makeroid</h4>
-	  <ul>
+	  <h2>Differences between MIT App Inventor and Makeroid</h2>
+	  <ul class="browser-default">
 		<li>Material Design</li>
 		<li>Enhanced User Interface</li>
 		<li>Live Chat Support</li>
 		<li>Extra component properties</li>
-			<ul>
+			<ul class="browser-default">
 				<li>WebViewer</li>
-					<ul>
+					<ul class="browser-default">
 						<li>Disable zoom (pinching and zoom buttons)</li>
 						<li>Disable zoom buttons</li>
 						<li>Zoom percentage</li>
 					</ul>
 				<li>ListView</li>
-					<ul>
+					<ul class="browser-default">
 						<li>Custom Filter Bar Hint</li>
 					</ul>
 				<li>Screen</li>
-					<ul>
+					<ul class="browser-default">
 						<li>Status and Navigation bar color</li>
 						<li>Custom device menu items</li>
 						<li>Move task to back</li>
@@ -37,14 +37,14 @@
 					</ul>
 			</ul>
 		<li>New components</li>
-			<ul>
+			<ul class="browser-default">
 				<li>Image Editor</li>
 				<li>Light Sensor</li>
 				<li>Google Map</li>
 				<li>Bluetooth Admin</li>
 			</ul>
 		<li>Plus, a lot more coming soon!</li>
-	  <ul>
+	  </ul>
 	</div>
 <?php include "../fab.php"; ?>
   </body>
