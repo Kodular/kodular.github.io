77d237e32cb728bf359871b451a76388a716870d
Diego Barreiro <diego.barreiro.perez@gmail.com>
2017-07-31 23:55:04 +0200

    Fix updater
    Finally

--- admin/update.php
+++ admin/update.php
@@ -1,8 +1,7 @@
 <?php
 
-exec('cd .. && sudo git pull 2>&1', $output);
+exec('cd .. && git pull 2>&1', $output);
+
 foreach($output  as $index=>$line) {
   echo ($index+1)."-> ".$line."<br>";
 }
-
-exec('cd /var/www && sudo chmod -R 755 html/ 2>&1', $output);
