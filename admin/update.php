68c45c9abd2c56e8901c9ffa481ccdbd4b27e9a8
Diego Barreiro <diego.barreiro.perez@gmail.com>
2017-07-31 17:11:29 +0200

    Init

--- admin/update.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-exec('cd .. && git pull 2>&1', $output);
-
-foreach($output  as $index=>$line) {
-  echo ($index+1)."-> ".$line."<br>";
-}
--- assets/templates/header.php
+++
@@ -1,34 +0,0 @@
-<?php include_once("config/min.php") ?>
-
-	<meta charset="utf-8" />
-	<meta name="theme-color" content="#4527a0">
-	<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-	<link rel="shortcut icon" href="/favicon.ico">
-	<link href="https://fonts.googleapis.com/css?family=Amaranth" rel="stylesheet">
-	<link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">	<link href="/assets/css/pace.css" rel="stylesheet" type="text/css">	<link href="/assets/css/materialize.min.css" rel="stylesheet" type="text/css">
-	<link href="/assets/css/style.css" rel="stylesheet" type="text/css">
-	<script type='application/ld+json'>
-	{
-	  "@context": "http://www.schema.org",
-	  "@type": "WebSite",
-	  "name": "Makeroid",
-	  "alternateName": "Makeroid App Builder",
-	  "url": "https://www.makeroid.tk"
-	}
-	</script>
-
-	<meta name="description" content="A new era of app building">
-
-	<meta property="og:title" content="Makeroid App Builder" />
-	<meta property="og:description" content="A new era of app building" />
-	<meta property="og:locale" content="en_US" />
-	<meta property="og:type" content="website" />
-	<meta property="og:url" content="https://www.makeroid.tk" />
-	<meta property="og:image" content="https://www.makeroid.tk/assets/logo.png" />
-  </head>
-
-	<body>
-    <?php include_once("config/analyticstracking.php") ?>
-	  <?php include_once("navbar.php") ?>
-
-    <?php include_once("config/ads.php"); ?>
