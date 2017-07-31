7d96dbefe49ed6b3a7b549390e94b7357394e806
Diego Barreiro <diego.barreiro.perez@gmail.com>
2017-07-31 23:53:06 +0200

    Update updater

--- admin/update.php
+++ admin/update.php
@@ -1,7 +1,8 @@
 <?php
 
-exec('cd .. && sudo git pull && cd .. && sudo chmod -R 755 html/ 2>&1', $output);
-
+exec('cd .. && sudo git pull 2>&1', $output);
 foreach($output  as $index=>$line) {
   echo ($index+1)."-> ".$line."<br>";
 }
+
+exec('cd /var/www && sudo chmod -R 755 html/ 2>&1', $output);
