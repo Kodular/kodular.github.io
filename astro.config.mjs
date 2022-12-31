c2406b7411db4aa237a882c726d71089420df8c2
Pavitra Golchha <pavitra@kodular.io>
2022-12-31 18:02:57 +0530

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
 import yaml from '@rollup/plugin-yaml';
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://www.kodular.io',
+  integrations: [sitemap()],
   vite: {
     plugins: [yaml()]
   }
