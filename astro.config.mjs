5c42a1925631cea2a69871cb9264e0e8ab4adee7
Pavitra Golchha <hello@pavi2410.me>
2025-05-11 19:27:15 +0530

    tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,12 +2,14 @@ import { defineConfig } from 'astro/config';
 import yaml from '@rollup/plugin-yaml';
 import sitemap from "@astrojs/sitemap";
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.kodular.io',
   integrations: [sitemap()],
   vite: {
-    plugins: [yaml()]
+    plugins: [yaml(), tailwindcss()]
   },
   redirects: {
     '/account': 'https://account.kodular.io/',
